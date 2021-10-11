1328432c45988d4a8097d42ff9d3d4fe648ceaf3
mingmonth <mingmonth@gmail.com>
2021-10-11 23:42:26 +0900

    feature: Add Topic, Message models

--- base/views.py
+++ base/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from .models import Room
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn python!'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend developers'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python!'},
+#     {'id': 2, 'name': 'Design with me'},
+#     {'id': 3, 'name': 'Frontend developers'},
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     # return HttpResponse('Home page')
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
@@ -17,9 +19,10 @@ def home(request):
 
 def room(request, pk):
     # return HttpResponse('ROOM')
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    # room = None
+    # for i in rooms:
+    #     if i['id'] == int(pk):
+    #         room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
